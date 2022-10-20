aeef1bfa0be1327972dbe728c48c96a85712378e
zplutor <zplutor@qq.com>
2022-10-20 13:20:28 +0800

    Implement VSCode extension.

--- tool/vscode_extension/src/extension.ts
+++ tool/vscode_extension/src/extension.ts
@@ -1,26 +1,31 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { DiscoverHost } from './DiscoverHost';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+let host = new DiscoverHost;
+
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "RunAnywhereVSCodeHost" is now active!');
+	console.log('Extension is activated.');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('RunAnywhereVSCodeHost.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from RunAnywhereVSCodeHost!');
-	});
+	if (vscode.window.state.focused) {
+		console.log('Window is focused, start host.');
+		host.start();
+	}
 
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(vscode.window.onDidChangeWindowState(state => {
+
+		console.log('Window focus state changed: ' + state.focused);
+
+		if (state.focused) {
+			host.start();
+		}
+		else {
+			host.stop();
+		}
+	}));
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+
+	host.stop();
+}
