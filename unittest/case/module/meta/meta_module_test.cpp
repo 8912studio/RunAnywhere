60376a890017ea81b0aed8beec5935d60baeda90
zplutor <zplutor@qq.com>
2023-08-15 00:23:53 +0800

    Add unittest for MetaModule.

--- unittest/case/module/meta/meta_module_test.cpp
+++ unittest/case/module/meta/meta_module_test.cpp
@@ -2,6 +2,7 @@
 #include "module/meta/meta_module.h"
 
 using namespace ra::mod::meta;
+using namespace ra::utility;
 
 TEST(MetaModuleTest, QuerySuggestedCommands) {
 
@@ -30,5 +31,49 @@ TEST(MetaModuleTest, QuerySuggestedCommands) {
     {
         auto result = meta_module.QuerySuggestedCommands(L"!abdddddddddddd");
         ASSERT_TRUE(result.empty());
+    }
+}
+
+
+TEST(MetaModuleTest, CreateCommand) {
+
+    MetaModule meta_module;
+
+    {
+        CommandLine command_line{ L"!about" };
+        auto command = meta_module.CreateCommand(command_line);
+        ASSERT_NE(command, nullptr);
+    }
+
+    {
+        CommandLine command_line{ L"!option" };
+        auto command = meta_module.CreateCommand(command_line);
+        ASSERT_NE(command, nullptr);
+    }
+
+    {
+        CommandLine command_line{ L"!reload" };
+        auto command = meta_module.CreateCommand(command_line);
+        ASSERT_NE(command, nullptr);
+    }
+
+    {
+        CommandLine command_line{ L"reload" };
+        auto command = meta_module.CreateCommand(command_line);
+        ASSERT_EQ(command, nullptr);
+    }
+
+    {
+        CommandLine command_line{ L"!nothing" };
+        auto command = meta_module.CreateCommand(command_line);
+        ASSERT_EQ(command, nullptr);
+    }
+
+    {
+        CommandLine command_line(L"\ufffc", [](int) {
+            return CommandLinePiece(CommandLinePieceType::TextBlock, L"!about");
+        });
+        auto command = meta_module.CreateCommand(command_line);
+        ASSERT_EQ(command, nullptr);
     }
 }
