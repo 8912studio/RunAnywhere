5c979e4a19b328198e53ce07de089ca2beae28ef
zplutor <zplutor@qq.com>
2023-09-30 22:17:48 +0800

    Use inline code for meta command header.

--- src/module/meta/meta_command.cpp
+++ src/module/meta/meta_command.cpp
@@ -31,7 +31,10 @@ CommandBrief MetaCommand::GetBrief() {
 std::shared_ptr<help::markdown::element::Element> MetaCommand::GetHelpContent() {
     
     return MakeRoot({
-        MakeHeader(HeaderDepth::_2, MetaCommandPrefix + command_info_.command + L" Command"),
+        MakeHeader(HeaderDepth::_2, {
+            MakeInlineCode(MetaCommandPrefix + command_info_.command),
+            MakeText(L" Command"),
+        }),
         MakeParagraph(command_info_.description),
     });
 }
