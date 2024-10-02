21bfd4456d1fd347f44d90087cf2dd87600468c6
Zplutor <zplutor@qq.com>
2024-10-03 02:00:18 +0800

    Add precompiled header.

---
+++ src/precompiled.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <Windows.h>
+#include <d2d1.h>
+#include <dwrite.h>
+#include <chrono>
+#include <filesystem>
+#include <functional>
+#include <map>
+#include <memory>
+#include <mutex>
+#include <optional>
+#include <set>
+#include <string>
+#include <string_view>
+#include <thread>
+#include <variant>
+#include <vector>
